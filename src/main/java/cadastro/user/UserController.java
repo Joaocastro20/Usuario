6b95c65ed2c6199dc2a324ec774b672c686d44fc
Joaocastro20 <98284593+Joaocastro20@users.noreply.github.com>
2022-04-09 19:36:24 -0300

    first commit

---
+++ src/main/java/cadastro/user/UserController.java
@@ -0,0 +1,35 @@
+package cadastro.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //Pegar dados no banco
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+    //deletar dados do banco
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public void findAll(@PathVariable Long id){
+        userRepository.deleteById(id);
+    }
+    //salvar dados no banco
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public User save(@RequestBody User user){
+        return userRepository.save(user);
+    }
+    //alterar dados no banco
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public User update(@RequestBody User user){
+        return userRepository.save(user);
+    }
+}
