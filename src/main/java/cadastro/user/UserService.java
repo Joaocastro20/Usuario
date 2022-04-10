4240bd84bfda1cc090798c28f01dbc873f5dd1d8
Joaocastro20 <98284593+Joaocastro20@users.noreply.github.com>
2022-04-10 17:33:23 -0300

    tirando dependencias inuteis

--- src/main/java/cadastro/user/UserService.java
+++ src/main/java/cadastro/user/UserService.java
@@ -1,10 +1,6 @@
 package cadastro.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
 
 @Service
 public class UserService{
